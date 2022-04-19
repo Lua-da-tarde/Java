00bdeb4ceda9a8668b34a1a924e640c51f62cc5c
Lua-da-tarde <luanna@paggiaro.com.br>
2022-04-19 17:33:18 -0300

    19/04

---
+++ Animais/Cachorro.java
@@ -0,0 +1,14 @@
+package bixanos;
+
+public class Cachorro extends Animal{
+    
+    public void correr(){
+    System.out.println("Sai que é sua cachorreira!");
+    }
+
+    @Override
+    public void emitirSom(){
+    System.out.println("Au au au au au au au au au au au au au au au au au au au au ...");
+    }
+    
+}
---
+++ Animais/Cavalo.java
@@ -0,0 +1,13 @@
+package bixanos;
+
+public class Cavalo extends Animal {
+    
+    public void correrA(){
+    System.out.println("[cavalo correndo]");
+    }
+
+    @Override
+    public void emitirSom(){
+    System.out.println("Iiiiiiirrrrí [bufa]");
+    }
+}
